780404ee636b5c57ecee00fe20106889f08941d9
paylayweb <soulaphon.vongmala559@gmail.com>
2025-02-21 10:06:11 +0700

    update dashboard

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\V1\back_end\venv
+set VIRTUAL_ENV=C:\Users\Dell\Desktop\LCICWEBSITE\back_end\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
