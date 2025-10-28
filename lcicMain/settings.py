f4db83015eae263790bc4e4858b6a3061d439df1
Sone11 <sonesdv164@gmail.com>
2025-10-28 11:21:23 +0700

    updaste

--- lcicMain/settings.py
+++ lcicMain/settings.py
@@ -134,6 +134,7 @@ DATABASES = {
          'connect_timeout': 30,  
             'options': '-c statement_timeout=600000'  
     },
+    # sql_server.pyodbc
     'utility': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'utility',
@@ -144,7 +145,7 @@ DATABASES = {
        
     },
     'ned_sql': {
-        'ENGINE': 'mssql',
+        'ENGINE': 'sql_server.pyodbc',
         'NAME': 'LCIC_Fingers_old',
         'USER': 'sa',
         'PASSWORD': 'Lcic@12345',
